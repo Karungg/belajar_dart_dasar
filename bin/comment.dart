ce4199cef5aa763e6ec5aa41341775eacf39ac43
Karungg <miftahfadilah71@gmail.com>
2024-03-19 13:25:42 +0700

    comment

---
+++ bin/comment.dart
@@ -0,0 +1,13 @@
+/// This is main function
+/// Will executed by dart
+void main()
+{
+  // this is name variable
+  var name = 'Miftah Fadilah';
+
+  /**
+   * this is multiple line comment
+   * you can add more here
+   */
+  print(name);
+}
