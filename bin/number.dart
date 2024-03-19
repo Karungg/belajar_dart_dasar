6dfdb258380b278747d4011653dd2f7465b8c09e
Karungg <miftahfadilah71@gmail.com>
2024-03-19 13:32:52 +0700

    number

---
+++ bin/number.dart
@@ -0,0 +1,17 @@
+void main()
+{
+
+  int number1 = 10;
+  double number2 = 10.5;
+
+  num number3 = 10;
+
+  print(number3);
+
+  number3 = 10.5;
+
+  print(number1);
+  print(number2);
+  print(number3);
+
+}
